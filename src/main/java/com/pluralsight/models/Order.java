a3339b17c00494b1b905c37ff0c95f62e6fc4dc1
duy nguyen <duybnguyen04@gmail.com>
2025-11-13 13:43:08 -0500

    ADD Order class: - Keeps track of user's order with the items arrayList

---
+++ src/main/java/com/pluralsight/models/Order.java
@@ -0,0 +1,32 @@
+package com.pluralsight.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private List<Item> items = new ArrayList<>();
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (Item item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+}
